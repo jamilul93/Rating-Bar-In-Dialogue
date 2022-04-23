94d445ea50aaeca9732c92aa97073c9322eec8cb
Jamilul93 <jami1807093@stud.kuet.ac.bd>
2022-04-24 02:27:46 +0600

    another java file is added

---
+++ Jami2.java
@@ -0,0 +1,17 @@
+class B{
+  int a;
+  int b;
+  public void set_a(int x){
+      a=x;
+  }
+  public void set_b(int y){
+      b=y;
+  }
+  public int get_a(){
+      return a;
+  }
+  public int get_b(){
+      return b;
+  }
+
+};
